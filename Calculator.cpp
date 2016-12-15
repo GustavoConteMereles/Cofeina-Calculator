cb8ba12e70af43e7392c647bcdd22785c346892e
GustavoConteMereles <gugumereles@hotmail.com>
2016-12-15 11:08:31 -0300

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,65 @@
+/*
+  Name: Cofeina Calculator 
+  Copyright: Gustavo Conte
+  Author: Gustavo Conte Mereles
+  Date: 14/12/16 18:06
+  Description: Calculadora simples e obejtiva, ultilizando lógica simples e organizada.
+*/
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <locale.h>
+
+
+int main() 
+{
+
+  setlocale(LC_ALL, "Portuguese");//habilita a acentuação para o português
+  system("color A");
+  printf("Bem Vindo Ao Cofeina Calculator\n\n");
+  printf("Ver. 1.0.0\n");
+  Sleep(1500);  //Exibe mensagem de boas vindas e espera 1 segundo
+ 
+  system("cls");
+   
+  system("color 7");  
+  int n1;
+  printf("Digite um número inteiro\n\n");
+  scanf("%d", &n1);  //Declara Variável que recebe o primeiro número do usuário
+  
+  system("cls");
+  
+  int n2;
+  printf("Agora digite outro\n\n");
+  scanf("%d", &n2);  //Declara Variável que recebe o segundo número do usuário
+
+  system("cls");
+  /*A partir deste ponto declaramos as variáveis inteiras para as operações e resultado em seguida realizamos as operacões*/
+  int soma, subt, mult;
+  
+  soma = n1 + n2;
+  subt = n1 - n2;
+  mult = n1 * n2;
+  
+  printf("O resultado da soma entre %d e %d é %d\n\n", n1, n2, soma);
+  printf("O resultado da subtração entre %d e %d é %d\n\n", n1, n2, subt);
+  printf("O resultado da mutiplicação entre %d e %d é %d\n\n", n1, n2, mult);
+  
+  Sleep(2000);  //Pausa do Software
+  int variavel;
+  variavel = getch();
+  switch(variavel){
+  
+  case '1': execute isso;break;
+  case '2': execute aquilo;break;
+  default: não execute nada;break;
+  
+  system("pause");
+  system("cls");
+    
+  printf("olá");
+  Sleep(5000);
+  return 0;
+}
---
+++ Cofeina_Calculator_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef COFEINA_CALCULATOR_PRIVATE_H
+#define COFEINA_CALCULATOR_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.0.1"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	""
+#define FILE_VERSION	""
+#define FILE_DESCRIPTION	"Cofeina Calculator"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Gustavo Conte"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"Cofeina Calculator"
+#define PRODUCT_VERSION	"1.0"
+
+#endif /*COFEINA_CALCULATOR_PRIVATE_H*/
